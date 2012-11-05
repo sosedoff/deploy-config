c8168cd694b6f9f3e15e6820c697cc166f62b739
Dan Sosedoff <dan.sosedoff@gmail.com>
2012-11-04 23:00:06 -0600

    Fix config test for xml

--- spec/deploy_config_spec.rb
+++ spec/deploy_config_spec.rb
@@ -3,6 +3,6 @@ require 'spec_helper'
 describe DeployConfig do
   it { should respond_to :load_file }
   it { should respond_to :from_json }
-  it { should respond_to :from_xml  }
   it { should respond_to :from_yml  }
+  it { should respond_to :from_yaml }
 end
